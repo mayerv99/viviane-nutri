6c4285209cf8d2ca20a3eb12c726c4c2ba80ef7c
Victor Mayer <mayerv99@gmail.com>
2025-11-11 18:41:01 -0300

    Adjust breakpoints in sections

--- src/sections/prices.tsx
+++ src/sections/prices.tsx
@@ -2,21 +2,21 @@ import Image from 'next/image'
 
 export default function PricesSection() {
     return (
-        <div className="flex flex-col lg:gap-0 gap-10 lg:flex-row justify-center py-10 lg-px0 px-5 relative bg-[#10635F]">
+        <div className="flex flex-col lg:gap-0 gap-10 justify-center py-10 lg-px0 px-5 relative bg-[#10635F]">
             <div className="absolute -top-12 left-0 w-full h-20 lg:h-32">
                 <Image src="/images/divider-teal.svg" alt="Divisor" fill />
             </div>
-            <div className="text-center flex flex-col gap-1 mt-10">
-                <span className="text-[50px] font-dynalight text-white my-0 leading-0">
+            <div className="text-center flex flex-col gap-1 mt-10 lg:mt-20">
+                <span className="text-[50px] lg:text-[100px] font-dynalight text-white my-0 leading-0">
                     Sua Transformação <br />
                 </span>
-                <span className="text-[43px] text-[#EAA98D] my-0">
+                <span className="text-[43px] text-[#EAA98D] my-0 lg:text-[80px]">
                     começa aqui
                 </span>
             </div>
-            <div className="flex flex-col gap-3">
-                <div className="w-3/4 mx-auto bg-[#FFBEA3] rounded-xl p-4 text-center flex flex-col gap-10">
-                    <h3 className="text-[32px] leading-6">
+            <div className="flex flex-col gap-3 text-[#502411] lg:flex-row lg:w-3/4 lg:mx-auto justify-center">
+                <div className="w-3/4 lg:w-1/4 xl:w-1/5 mx-auto lg:mx-0 bg-[#FFBEA3] rounded-xl p-4 text-center flex flex-col gap-10 2xl:w-1/6">
+                    <h3 className="text-[32px] leading-6 font-semibold">
                         Pacote Promocional
                     </h3>
                     <div>
@@ -43,18 +43,24 @@ export default function PricesSection() {
                                 />
                             </div>
                         </div>
-                        <span className="text-[22px]">3 Consultas</span>
+                        <span className="text-[22px] uppercase">
+                            3 Consultas
+                        </span>
                     </div>
                     <div>
                         <span className="text-[12px]">de R$ 750,00</span> <br />
-                        <span className="text-[38px]">R$ 600,00</span>
+                        <span className="text-[38px] font-semibold">
+                            R$ 600,00
+                        </span>
                     </div>
-                    <button className="rounded-xl py-1 bg-primary text-white">
+                    <button className="rounded-xl py-1 bg-primary text-white mt-auto">
                         Agendar consulta
                     </button>
                 </div>
-                <div className="w-3/4 mx-auto bg-white rounded-xl p-4 text-center flex flex-col gap-10">
-                    <h3 className="text-[32px] leading-6">Pacote Individual</h3>
+                <div className="w-3/4 mx-auto lg:mx-0 bg-white rounded-xl p-4 text-center flex flex-col gap-10 lg:w-1/4 xl:w-1/5 2xl:w-1/6">
+                    <h3 className="text-[32px] leading-6 font-semibold">
+                        Pacote Individual
+                    </h3>
                     <div>
                         <div className="flex justify-center">
                             <div className="w-4 h-4 relative">
@@ -65,16 +71,18 @@ export default function PricesSection() {
                                 />
                             </div>
                         </div>
-                        <span className="text-[22px]">1 Consulta</span>
+                        <span className="text-[22px] uppercase">
+                            1 Consulta
+                        </span>
                     </div>
-                    <div>
-                        <span className="text-[38px]">R$ 250,00</span>
+                    <div className="text-[38px] font-semibold mt-auto">
+                        R$ 250,00
                     </div>
                     <button className="rounded-xl py-1 bg-primary text-white">
                         Agendar consulta
                     </button>
                 </div>
-                <span className="text-white text-center text-[16px] font-bold mt-8">
+                <span className="text-white text-center text-[16px] font-bold mt-8 lg:hidden">
                     Formas de pagamento: <br />
                     Dinheiro | PIX | Crédito | Débito
                 </span>
